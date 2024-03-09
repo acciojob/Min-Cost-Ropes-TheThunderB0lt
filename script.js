b111f45a585e6c8eb2af74a084bb1fc3497a875f
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-09 20:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr){ 
+	arr.sort(function(a,b){return a-b;});
+	let result = 0;
+	
+	while(arr.length > 1){
+		let first = arr.shift();
+		let second = arr.shift();
+
+		result += first + second;
+		arr.push(first+second);
+		arr.sort(function(a,b){return a-b;});
+	  }
+	return result;
 }
 
-module.exports=mincost;
+module.exports = mincost;
